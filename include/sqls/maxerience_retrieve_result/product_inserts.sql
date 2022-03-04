43f8095d3ad8e37a71ef2bd894d126ac0eac40c7
Adrian Parry <subparry@gmail.com>
2022-03-04 14:21:00 -0300

    Add empty and foreign product ids

--- include/sqls/maxerience_retrieve_result/product_inserts.sql
+++ include/sqls/maxerience_retrieve_result/product_inserts.sql
@@ -9,6 +9,41 @@ INSERT INTO product (
     modified_by_user_id,
     modified_on
 )
+VALUES (
+    18,
+    '{"name": "Empty", "sku": ""}',
+    '{}',
+    '{}',
+    '{}',
+    1,
+    now() :: timestamp,
+    1,
+    now():: timestamp
+       ), (
+    54,
+    '{"name": "Foreign", "sku": "0"}',
+    '{}',
+    '{}',
+    '{}',
+    1,
+    now() :: timestamp,
+    1,
+    now():: timestamp
+)
+ON CONFLICT(id) DO NOTHING
+;
+
+INSERT INTO product (
+    id,
+    identity,
+    display,
+    details,
+    recognition_details,
+    created_by_user_id,
+    created_on,
+    modified_by_user_id,
+    modified_on
+)
 VALUES %s
 ON CONFLICT(id) DO UPDATE SET
     identity = EXCLUDED.identity,
