49cf6bf448a7bbe7c4fd92706c71ce9db019a856
Adrian Parry <subparry@gmail.com>
2022-03-10 13:40:01 -0300

    Add correct scene and subscene ids according to maxerience support

--- dags/base/utils/ml_scene_info.py
+++ dags/base/utils/ml_scene_info.py
@@ -1,11 +1,11 @@
 def extract_info_from_question_heading(heading):
     if 'ambiente Embonor' in heading:
-        return {'scene': 2, 'sub_scene': 3}
+        return {'scene': 2, 'sub_scene': 1}
     elif 'ambiente de la competencia' in heading:
-        return {'scene': 2, 'sub_scene': 4}
+        return {'scene': 2, 'sub_scene': 2}
     elif 'equipos de frío Embonor' in heading:
-        return {'scene': 1, 'sub_scene': 3}
+        return {'scene': 1, 'sub_scene': 1}
     elif 'equipos de frío de la competencia' in heading:
-        return {'scene': 1, 'sub_scene': 4}
+        return {'scene': 1, 'sub_scene': 2}
     else:
         raise ValueError(f'Unrecognized scene and sub_scene from question heading: {heading}')
