548a53beeda911cd5fa319e12de1dafa551203a2
Adrian Parry <subparry@gmail.com>
2022-03-23 16:50:07 -0300

    transform broken hierarchy query result into csv

--- dags/base/expos_service/send_broken_hierarchy_data.py
+++ dags/base/expos_service/send_broken_hierarchy_data.py
@@ -10,6 +10,11 @@ def send_broken_hierarchy_data(job_id):
     if len(data) == 0:
         pass  # TODO: send just a message
     else:
+        csv_data = 'vendor_source_id,vendor_name,vendor_rut,vendor_email,vendor_phone,branch_office_name,' \
+                   'plant_name,last_evaluation_at,evaluations_total\n'
+        for row in data:
+            csv_data += ','.join(row)
+            csv_data += '\n'
         send_file_content_to_channels(file_content=json.dumps(data), channels=['C036G7G8GJ2'],
                                       initial_comment='Aquí está el reporte de vendedores sin supervisor '
                                                       'correspondiente al ETL de hoy :spiral_note_pad:',
