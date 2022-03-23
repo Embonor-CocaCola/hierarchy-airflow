266bcba3dd1f05041755de2132cd398d4c553bab
Adrian Parry <subparry@gmail.com>
2022-03-23 17:22:20 -0300

    parameterize etl success channel and fix issue with csv creation

--- dags/base/expos_service/send_broken_hierarchy_data.py
+++ dags/base/expos_service/send_broken_hierarchy_data.py
@@ -2,6 +2,7 @@ import json
 
 from base.utils.query_with_return import parameterized_query
 from base.utils.slack import send_file_content_to_channels
+from config.common.settings import SLACK_ETL_SUCCESS_CHANNEL
 
 
 def send_broken_hierarchy_data(job_id):
@@ -13,9 +14,9 @@ def send_broken_hierarchy_data(job_id):
         csv_data = 'vendor_source_id,vendor_name,vendor_rut,vendor_email,vendor_phone,branch_office_name,' \
                    'plant_name,last_evaluation_at,evaluations_total\n'
         for row in data:
-            csv_data += ','.join(row)
+            csv_data += ','.join(map(lambda entry: str(entry), row))
             csv_data += '\n'
-        send_file_content_to_channels(file_content=json.dumps(data), channels=['C036G7G8GJ2'],
+        send_file_content_to_channels(file_content=json.dumps(data), channels=[SLACK_ETL_SUCCESS_CHANNEL],
                                       initial_comment='Aquí está el reporte de vendedores sin supervisor '
                                                       'correspondiente al ETL de hoy :spiral_note_pad:',
                                       title='reporte_vendedores_sin_jerarquia.txt')
