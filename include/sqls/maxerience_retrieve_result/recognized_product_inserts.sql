faba5e2e2336f5bfbf2b212c530fe62cd8c097f3
Adrian Parry <subparry@gmail.com>
2022-03-26 00:33:47 -0300

    Removed self join

--- include/sqls/maxerience_retrieve_result/recognized_product_inserts.sql
+++ include/sqls/maxerience_retrieve_result/recognized_product_inserts.sql
@@ -42,15 +42,15 @@ INSERT INTO recognized_product (
              INNER JOIN product p ON input_rows.product_id = p.id
              INNER JOIN survey s ON input_rows.survey_id :: uuid = s.id
              INNER JOIN analyzed_photo ap ON input_rows.analyzed_photo_id ::uuid = ap.id
-             LEFT JOIN recognized_product target ON
-                target.analyzed_photo_id = input_rows.analyzed_photo_id::uuid
-            AND target.survey_id = input_rows.survey_id::uuid
-            AND target.re_id = input_rows.re_id::text
-            AND target.position::jsonb = input_rows.position::jsonb
-            AND target.external_id = input_rows.external_id ::integer
-            AND target.block_id = input_rows.block_id ::text
-            AND target.product_id = input_rows.product_id::integer
-            AND target.single_facings = input_rows.single_facings::integer
-            AND target.is_foreign = input_rows.is_foreign::boolean
-    WHERE target.id IS NULL
+{#             LEFT JOIN recognized_product target ON#}
+{#                target.analyzed_photo_id = input_rows.analyzed_photo_id::uuid#}
+{#            AND target.survey_id = input_rows.survey_id::uuid#}
+{#            AND target.re_id = input_rows.re_id::text#}
+{#            AND target.position::jsonb = input_rows.position::jsonb#}
+{#            AND target.external_id = input_rows.external_id ::integer#}
+{#            AND target.block_id = input_rows.block_id ::text#}
+{#            AND target.product_id = input_rows.product_id::integer#}
+{#            AND target.single_facings = input_rows.single_facings::integer#}
+{#            AND target.is_foreign = input_rows.is_foreign::boolean#}
+{#    WHERE target.id IS NULL#}
 );
