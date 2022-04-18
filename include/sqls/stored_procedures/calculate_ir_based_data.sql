c31d13b4670fb3c7db9b4a83b07fe5eb0ba57c18
Adrian Parry <44247645+subparry@users.noreply.github.com>
2022-04-18 17:57:56 -0400

    change jsonb references to actual columns (#69)

--- include/sqls/stored_procedures/calculate_ir_based_data.sql
+++ include/sqls/stored_procedures/calculate_ir_based_data.sql
@@ -3,7 +3,7 @@ CREATE OR REPLACE PROCEDURE calculate_ir_based_data()
 $$
 with compliance as (
     select count(rp.id) filter (
-        where NULLIF((p.identity ->> 'sku'), '')::integer IN (select * from unnest(spp.skus))
+        where (p.sku) IN (select * from unnest(spp.skus))
         ) >= spp.required_facings achieves,
            spp.is_essential,
            s.id                   survey_id
@@ -61,163 +61,163 @@ INTO preprocessed_ir (is_pure,
                       essentials_compliant,
                       survey_id)
 SELECT CASE count(distinct p.id) FILTER ( WHERE ap.scene_type = '1' AND ap.sub_scene_type = '1' AND
-                                                p.recognition_details ->> 'is_foreign' = 'true')
+                                                p.is_foreign is true)
            WHEN 0 THEN true
            ELSE false END
                                                                                       is_pure,
        count(distinct p.id) FILTER ( WHERE ap.scene_type = '1' AND ap.sub_scene_type = '1' AND
-                                           (p.recognition_details ->> 'is_foreign' != 'true' OR p.id = 54))
+                                           (p.is_foreign is not true OR p.id = 54))
                                                                                       co_cooler_co_products,
        count(distinct p.id) FILTER ( WHERE ap.scene_type = '1' AND ap.sub_scene_type = '1' AND
-                                           (p.recognition_details ->> 'is_foreign' = 'true' OR p.id = 18))
+                                           (p.is_foreign is true OR p.id = 18))
                                                                                       co_cooler_non_co_products,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' != 'true' AND p.details ->> 'group' like 'SS%' AND
-                      p.details ->> 'category' = 'AGUA' AND p.details ->> 'local_category_name' != 'SABORIZADA')
+       FILTER ( WHERE p.is_foreign is not true AND p.group like 'SS%' AND
+                      p.category = 'AGUA' AND p.local_category_name != 'SABORIZADA')
                                                                                       stills_ss_water_unflavored_co,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' != 'true' AND p.details ->> 'group' like 'MS%' AND
-                      p.details ->> 'category' = 'AGUA' AND p.details ->> 'local_category_name' != 'SABORIZADA')
+       FILTER ( WHERE p.is_foreign is not true AND p.group like 'MS%' AND
+                      p.category = 'AGUA' AND p.local_category_name != 'SABORIZADA')
                                                                                       stills_ms_water_unflavored_co,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' = 'true' AND p.details ->> 'group' like 'SS%' AND
-                      p.details ->> 'category' = 'AGUA' AND p.details ->> 'local_category_name' != 'SABORIZADA')
+       FILTER ( WHERE p.is_foreign is true AND p.group like 'SS%' AND
+                      p.category = 'AGUA' AND p.local_category_name != 'SABORIZADA')
                                                                                       stills_ss_water_unflavored_pe,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' = 'true' AND p.details ->> 'group' like 'MS%' AND
-                      p.details ->> 'category' = 'AGUA' AND p.details ->> 'local_category_name' != 'SABORIZADA')
+       FILTER ( WHERE p.is_foreign is true AND p.group like 'MS%' AND
+                      p.category = 'AGUA' AND p.local_category_name != 'SABORIZADA')
                                                                                       stills_ms_water_unflavored_pe,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' != 'true' AND p.details ->> 'group' like 'SS%' AND
-                      p.details ->> 'category' = 'AGUA' AND p.details ->> 'local_category_name' = 'SABORIZADA')
+       FILTER ( WHERE p.is_foreign is not true AND p.group like 'SS%' AND
+                      p.category = 'AGUA' AND p.local_category_name = 'SABORIZADA')
                                                                                       stills_ss_water_flavored_co,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' != 'true' AND p.details ->> 'group' like 'MS%' AND
-                      p.details ->> 'category' = 'AGUA' AND p.details ->> 'local_category_name' = 'SABORIZADA')
+       FILTER ( WHERE p.is_foreign is not true AND p.group like 'MS%' AND
+                      p.category = 'AGUA' AND p.local_category_name = 'SABORIZADA')
                                                                                       stills_ms_water_flavored_co,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' = 'true' AND p.details ->> 'group' like 'SS%' AND
-                      p.details ->> 'category' = 'AGUA' AND p.details ->> 'local_category_name' = 'SABORIZADA')
+       FILTER ( WHERE p.is_foreign is true AND p.group like 'SS%' AND
+                      p.category = 'AGUA' AND p.local_category_name = 'SABORIZADA')
                                                                                       stills_ss_water_flavored_pe,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' = 'true' AND p.details ->> 'group' like 'MS%' AND
-                      p.details ->> 'category' = 'AGUA' AND p.details ->> 'local_category_name' = 'SABORIZADA')
+       FILTER ( WHERE p.is_foreign is true AND p.group like 'MS%' AND
+                      p.category = 'AGUA' AND p.local_category_name = 'SABORIZADA')
                                                                                       stills_ms_water_flavored_pe,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' != 'true' AND p.details ->> 'group' like 'SS%' AND
-                      p.details ->> 'category' = 'NCB' AND p.details ->> 'local_category_name' = 'ENERGIZANTES')
+       FILTER ( WHERE p.is_foreign is not true AND p.group like 'SS%' AND
+                      p.category = 'NCB' AND p.local_category_name = 'ENERGIZANTES')
                                                                                       stills_ss_ncbs_energy_co,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' != 'true' AND p.details ->> 'group' like 'MS%' AND
-                      p.details ->> 'category' = 'NCB' AND p.details ->> 'local_category_name' = 'ENERGIZANTES')
+       FILTER ( WHERE p.is_foreign is not true AND p.group like 'MS%' AND
+                      p.category = 'NCB' AND p.local_category_name = 'ENERGIZANTES')
                                                                                       stills_ms_ncbs_energy_co,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' = 'true' AND p.details ->> 'group' like 'SS%' AND
-                      p.details ->> 'category' = 'NCB' AND p.details ->> 'local_category_name' = 'ENERGIZANTES')
+       FILTER ( WHERE p.is_foreign is true AND p.group like 'SS%' AND
+                      p.category = 'NCB' AND p.local_category_name = 'ENERGIZANTES')
                                                                                       stills_ss_ncbs_energy_pe,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' = 'true' AND p.details ->> 'group' like 'MS%' AND
-                      p.details ->> 'category' = 'NCB' AND p.details ->> 'local_category_name' = 'ENERGIZANTES')
+       FILTER ( WHERE p.is_foreign is true AND p.group like 'MS%' AND
+                      p.category = 'NCB' AND p.local_category_name = 'ENERGIZANTES')
                                                                                       stills_ms_ncbs_energy_pe,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' != 'true' AND p.details ->> 'group' like 'SS%' AND
-                      p.details ->> 'category' = 'NCB' AND
-                      p.details ->> 'local_category_name' IN ('JUGOS', 'KAPO', 'LACTEOS'))
+       FILTER ( WHERE p.is_foreign is not true AND p.group like 'SS%' AND
+                      p.category = 'NCB' AND
+                      p.local_category_name IN ('JUGOS', 'KAPO', 'LACTEOS'))
                                                                                       stills_ss_ncbs_juice_co,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' != 'true' AND p.details ->> 'group' like 'MS%' AND
-                      p.details ->> 'category' = 'NCB' AND
-                      p.details ->> 'local_category_name' IN ('JUGOS', 'KAPO', 'LACTEOS'))
+       FILTER ( WHERE p.is_foreign is not true AND p.group like 'MS%' AND
+                      p.category = 'NCB' AND
+                      p.local_category_name IN ('JUGOS', 'KAPO', 'LACTEOS'))
                                                                                       stills_ms_ncbs_juice_co,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' = 'true' AND p.details ->> 'group' like 'SS%' AND
-                      p.details ->> 'category' = 'NCB' AND
-                      p.details ->> 'local_category_name' IN ('JUGOS', 'KAPO', 'LACTEOS'))
+       FILTER ( WHERE p.is_foreign is true AND p.group like 'SS%' AND
+                      p.category = 'NCB' AND
+                      p.local_category_name IN ('JUGOS', 'KAPO', 'LACTEOS'))
                                                                                       stills_ss_ncbs_juice_pe,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' = 'true' AND p.details ->> 'group' like 'MS%' AND
-                      p.details ->> 'category' = 'NCB' AND
-                      p.details ->> 'local_category_name' IN ('JUGOS', 'KAPO', 'LACTEOS'))
+       FILTER ( WHERE p.is_foreign is true AND p.group like 'MS%' AND
+                      p.category = 'NCB' AND
+                      p.local_category_name IN ('JUGOS', 'KAPO', 'LACTEOS'))
                                                                                       stills_ms_ncbs_juice_pe,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' != 'true' AND p.details ->> 'group' like 'SS%' AND
-                      p.details ->> 'category' = 'NCB' AND p.details ->> 'local_category_name' = 'SPORT DRINK')
+       FILTER ( WHERE p.is_foreign is not true AND p.group like 'SS%' AND
+                      p.category = 'NCB' AND p.local_category_name = 'SPORT DRINK')
                                                                                       stills_ss_ncbs_sports_co,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' != 'true' AND p.details ->> 'group' like 'MS%' AND
-                      p.details ->> 'category' = 'NCB' AND p.details ->> 'local_category_name' = 'SPORT DRINK')
+       FILTER ( WHERE p.is_foreign is not true AND p.group like 'MS%' AND
+                      p.category = 'NCB' AND p.local_category_name = 'SPORT DRINK')
                                                                                       stills_ms_ncbs_sports_co,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' = 'true' AND p.details ->> 'group' like 'SS%' AND
-                      p.details ->> 'category' = 'NCB' AND p.details ->> 'local_category_name' = 'SPORT DRINK')
+       FILTER ( WHERE p.is_foreign is true AND p.group like 'SS%' AND
+                      p.category = 'NCB' AND p.local_category_name = 'SPORT DRINK')
                                                                                       stills_ss_ncbs_sports_pe,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' = 'true' AND p.details ->> 'group' like 'MS%' AND
-                      p.details ->> 'category' = 'NCB' AND p.details ->> 'local_category_name' = 'SPORT DRINK')
+       FILTER ( WHERE p.is_foreign is true AND p.group like 'MS%' AND
+                      p.category = 'NCB' AND p.local_category_name = 'SPORT DRINK')
                                                                                       stills_ms_ncbs_sports_pe,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' != 'true' AND p.details ->> 'category' = 'SSD' AND
-                      p.details ->> 'group' = 'SS RET')
+       FILTER ( WHERE p.is_foreign is not true AND p.category = 'SSD' AND
+                      p.group = 'SS RET')
                                                                                       ssd_ss_ret_co,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' = 'true' AND p.details ->> 'category' = 'SSD' AND
-                      p.details ->> 'group' = 'SS RET')
+       FILTER ( WHERE p.is_foreign is true AND p.category = 'SSD' AND
+                      p.group = 'SS RET')
                                                                                       ssd_ss_ret_pe,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' != 'true' AND p.details ->> 'category' = 'SSD' AND
-                      p.details ->> 'group' = 'SS OW')
+       FILTER ( WHERE p.is_foreign is not true AND p.category = 'SSD' AND
+                      p.group = 'SS OW')
                                                                                       ssd_ss_ow_co,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' = 'true' AND p.details ->> 'category' = 'SSD' AND
-                      p.details ->> 'group' = 'SS OW')
+       FILTER ( WHERE p.is_foreign is true AND p.category = 'SSD' AND
+                      p.group = 'SS OW')
                                                                                       ssd_ss_ow_pe,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' != 'true' AND p.details ->> 'category' = 'SSD' AND
-                      p.details ->> 'group' like 'SS%' AND p.details ->> 'flavour_name' = 'Cola')
+       FILTER ( WHERE p.is_foreign is not true AND p.category = 'SSD' AND
+                      p.group like 'SS%' AND p.flavour_name = 'Cola')
                                                                                       ssd_ss_cola_co,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' = 'true' AND p.details ->> 'category' = 'SSD' AND
-                      p.details ->> 'group' like 'SS%' AND p.details ->> 'flavour_name' = 'Cola')
+       FILTER ( WHERE p.is_foreign is true AND p.category = 'SSD' AND
+                      p.group like 'SS%' AND p.flavour_name = 'Cola')
                                                                                       ssd_ss_cola_pe,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' != 'true' AND p.details ->> 'category' = 'SSD' AND
-                      p.details ->> 'group' like 'SS%' AND p.details ->> 'flavour_name' != 'Cola')
+       FILTER ( WHERE p.is_foreign is not true AND p.category = 'SSD' AND
+                      p.group like 'SS%' AND p.flavour_name != 'Cola')
                                                                                       ssd_ss_flavor_co,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' = 'true' AND p.details ->> 'category' = 'SSD' AND
-                      p.details ->> 'group' like 'SS%' AND p.details ->> 'flavour_name' != 'Cola')
+       FILTER ( WHERE p.is_foreign is true AND p.category = 'SSD' AND
+                      p.group like 'SS%' AND p.flavour_name != 'Cola')
                                                                                       ssd_ss_flavor_pe,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' != 'true' AND p.details ->> 'category' = 'SSD' AND
-                      p.details ->> 'group' = 'MS RET')
+       FILTER ( WHERE p.is_foreign is not true AND p.category = 'SSD' AND
+                      p.group = 'MS RET')
                                                                                       ssd_ms_ret_co,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' = 'true' AND p.details ->> 'category' = 'SSD' AND
-                      p.details ->> 'group' = 'MS RET')
+       FILTER ( WHERE p.is_foreign is true AND p.category = 'SSD' AND
+                      p.group = 'MS RET')
                                                                                       ssd_ms_ret_pe,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' != 'true' AND p.details ->> 'category' = 'SSD' AND
-                      p.details ->> 'group' = 'MS OW')
+       FILTER ( WHERE p.is_foreign is not true AND p.category = 'SSD' AND
+                      p.group = 'MS OW')
                                                                                       ssd_ms_ow_co,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' = 'true' AND p.details ->> 'category' = 'SSD' AND
-                      p.details ->> 'group' = 'MS OW')
+       FILTER ( WHERE p.is_foreign is true AND p.category = 'SSD' AND
+                      p.group = 'MS OW')
                                                                                       ssd_ms_ow_pe,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' != 'true' AND p.details ->> 'category' = 'SSD' AND
-                      p.details ->> 'group' like 'MS%' AND p.details ->> 'flavour_name' = 'Cola')
+       FILTER ( WHERE p.is_foreign is not true AND p.category = 'SSD' AND
+                      p.group like 'MS%' AND p.flavour_name = 'Cola')
                                                                                       ssd_ms_cola_co,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' = 'true' AND p.details ->> 'category' = 'SSD' AND
-                      p.details ->> 'group' like 'MS%' AND p.details ->> 'flavour_name' = 'Cola')
+       FILTER ( WHERE p.is_foreign is true AND p.category = 'SSD' AND
+                      p.group like 'MS%' AND p.flavour_name = 'Cola')
                                                                                       ssd_ms_cola_pe,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' != 'true' AND p.details ->> 'category' = 'SSD' AND
-                      p.details ->> 'group' like 'MS%' AND p.details ->> 'flavour_name' != 'Cola')
+       FILTER ( WHERE p.is_foreign is not true AND p.category = 'SSD' AND
+                      p.group like 'MS%' AND p.flavour_name != 'Cola')
                                                                                       ssd_ms_flavor_co,
        count(distinct p.id)
-       FILTER ( WHERE p.recognition_details ->> 'is_foreign' = 'true' AND p.details ->> 'category' = 'SSD' AND
-                      p.details ->> 'group' like 'MS%' AND p.details ->> 'flavour_name' != 'Cola')
+       FILTER ( WHERE p.is_foreign is true AND p.category = 'SSD' AND
+                      p.group like 'MS%' AND p.flavour_name != 'Cola')
                                                                                       ssd_ms_flavor_pe,
        bool_and(compliance.achieves)                                                  success_photo_compliant,
 
