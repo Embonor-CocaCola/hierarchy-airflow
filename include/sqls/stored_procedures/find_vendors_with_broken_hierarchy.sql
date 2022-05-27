70fc3032841aa9d49dc3e256359ef2beb4ec2609
Adrian Parry <subparry@gmail.com>
2022-05-27 12:55:43 -0400

    add vendors without surveys to report

--- include/sqls/stored_procedures/find_vendors_with_broken_hierarchy.sql
+++ include/sqls/stored_procedures/find_vendors_with_broken_hierarchy.sql
@@ -21,22 +21,20 @@ CREATE FUNCTION find_vendors_with_broken_hierarchy(jobid int)
             boc.name,
             pc.name,
             MAX(sc.external_created_at)::text,
-            count(sc.id)
+            COALESCE(count(sc.id), 0)
         FROM airflow.vendor_conform vc
             INNER JOIN airflow.branch_office_conform boc ON vc.branch_office_id = boc.source_id
             INNER JOIN airflow.plant_conform pc ON boc.plant_id = pc.source_id
-            INNER JOIN airflow.survey_conform sc on sc.vendor_id = vc.source_id
+            LEFT JOIN airflow.survey_conform sc on sc.vendor_id = vc.source_id
         WHERE vc.source_id IN
             (
-                SELECT distinct SC.vendor_id FROM airflow.survey_conform SC WHERE vendor_id IN (
-                SELECT distinct ON (VEC.source_id) VEC.source_id FROM airflow.vendor_conform VEC
-                LEFT JOIN airflow.vendor_plant_conform VPC ON
-                    substr(VEC.rut, 1, length(COALESCE(NULLIF(VEC.rut, ''), ' ')) - 1) = VPC.vendor_id :: TEXT
-                WHERE
-                VEC.job_id = jobid AND
-                SC.job_id = jobid AND
-                VPC.id IS NULL
-            )
+              SELECT distinct ON (VEC.source_id) VEC.source_id FROM airflow.vendor_conform VEC
+                    LEFT JOIN airflow.vendor_plant_conform VPC ON
+                        substr(VEC.rut, 1, length(COALESCE(NULLIF(VEC.rut, ''), ' ')) - 1) = VPC.vendor_id :: TEXT
+                    WHERE
+                    VEC.job_id = jobid AND
+                    SC.job_id = jobid AND
+                    VPC.id IS NULL
         )
         AND vc.job_id = jobid
         AND boc.job_id = jobid
