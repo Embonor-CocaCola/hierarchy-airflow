0f4490b9fd29f9f816fb6e53f60877d45a316be6
Adrian Parry <subparry@gmail.com>
2022-06-14 10:14:29 -0400

    Add procedure to calculate survey_photo_score

--- include/sqls/stored_procedures/calculate_survey_metadata.sql
+++ include/sqls/stored_procedures/calculate_survey_metadata.sql
@@ -1,3 +1,4 @@
+DROP PROCEDURE IF EXISTS calculate_survey_metadata();
 CREATE OR REPLACE PROCEDURE calculate_survey_metadata()
     LANGUAGE SQL AS $$
         INSERT INTO survey_metadata (
@@ -5,6 +6,10 @@ CREATE OR REPLACE PROCEDURE calculate_survey_metadata()
             empty_value,
             ir_photos,
             days_since_last_survey,
+            ambient_company_photos,
+            ambient_competitor_photos,
+            cooler_company_photos,
+            cooler_competitor_photos,
             survey_id
         )
         SELECT
@@ -12,6 +17,10 @@ CREATE OR REPLACE PROCEDURE calculate_survey_metadata()
             (ps.empty_products::float4 / nullif(ps.total_products, 0))::float4 foreign_value,
             sum(array_length(a.attachments, 1)) filter (where q.heading like 'Fotos%' or q.heading ilike '%cuatripendón%'),
             s.created_at::date - (select s2.created_at::date from survey s2 where s2.customer_id = s.customer_id and s2.created_at < s.created_at order by s2.created_at DESC limit 1) days_since_last_survey,
+            coalesce(sum(array_length(a.attachments, 1)) filter (where q.heading ilike 'Fotos%ambiente%Embonor%'),0),
+            coalesce(sum(array_length(a.attachments, 1)) filter (where q.heading ilike 'Fotos%ambiente%competencia%'),0),
+            coalesce(sum(array_length(a.attachments, 1)) filter (where q.heading ilike 'Fotos%frío%embonor%'),0),
+            coalesce(sum(array_length(a.attachments, 1)) filter (where q.heading ilike 'Fotos%frío%competencia%'),0),
             s.id survey_id
         FROM survey s
             INNER JOIN preprocessed_sovi ps on s.id = ps.survey_id
