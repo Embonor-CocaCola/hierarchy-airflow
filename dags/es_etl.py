fbc2134b4f77dc8a894a79139045ab04c7507b14
Adrian Parry <subparry@gmail.com>
2022-06-23 17:38:06 -0400

    etl in development extracts from staging

--- dags/es_etl.py
+++ dags/es_etl.py
@@ -1,4 +1,5 @@
 from airflow import DAG
 from base.expos_service.etl_dag_factory import EtlDagFactory
+from config.expos_service.settings import ES_STAGE
 
-dag: DAG = EtlDagFactory().build()
+dag: DAG = EtlDagFactory(check_run=ES_STAGE == 'development').build()
