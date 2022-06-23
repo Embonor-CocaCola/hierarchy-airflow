01d59f41d65aeee0285213bcf254d9cb77d70e2e
Adrian Parry <subparry@gmail.com>
2022-06-23 16:46:34 -0400

    fix day interval for reset survey queries

--- include/sqls/reset_rejected_surveys/get_surveys_to_reset.sql
+++ include/sqls/reset_rejected_surveys/get_surveys_to_reset.sql
@@ -4,5 +4,5 @@ FROM survey s
 INNER JOIN vendor v on v.id = s.vendor_id
 INNER JOIN customer c on c.id = s.customer_id
 INNER JOIN evaluation_status es on s.evaluation_status_id = es.id
-WHERE s.created_at between now() - interval '23 days' and now()
+WHERE s.created_at between now() - interval '1 day' and now()
     AND s.reset_at IS NULL;
