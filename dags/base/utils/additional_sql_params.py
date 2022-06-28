5145e542e551409f7ac3a44de4956384af83d5ab
Adrian Parry <subparry@gmail.com>
2022-06-28 13:21:33 -0400

    Change additional_columns for development to load staging data

--- dags/base/utils/additional_sql_params.py
+++ dags/base/utils/additional_sql_params.py
@@ -2,14 +2,17 @@ additional_params = {
     'development.surveys.temp': {
         'additional_columns':
         """
+            answers_goal_number TEXT,
+            country TEXT,
             created_at TEXT,
+            is_required TEXT,
             name TEXT,
             paused TEXT,
-            period TEXT,
             portals TEXT,
             updated_at TEXT,
             valid_since TEXT,
-            valid_until TEXT
+            valid_until TEXT,
+            workgroup TEXT
         """,
     },
     'staging.surveys.temp': {
