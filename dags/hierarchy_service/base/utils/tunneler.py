ef4e537bc4d5908efc2075c688b3129aa106ef5f
Adrian Parry <44247645+subparry@users.noreply.github.com>
2022-08-03 22:34:52 -0400

    remove broken import (#3)

--- dags/hierarchy_service/base/utils/tunneler.py
+++ dags/hierarchy_service/base/utils/tunneler.py
@@ -3,12 +3,12 @@ from time import sleep
 
 from airflow.providers.ssh.hooks.ssh import SSHHook
 
-from hierarchy_service.config.etl.settings import ES_SSH_CONN_ID
+from hierarchy_service.config.etl.settings import HIERARCHY_SSH_CONN_ID
 
 
 class Tunneler:
     def __init__(self, remote_port, remote_host, local_port):
-        self.hook = SSHHook(ssh_conn_id=ES_SSH_CONN_ID)
+        self.hook = SSHHook(ssh_conn_id=HIERARCHY_SSH_CONN_ID)
         self.tunnel = self.hook.get_tunnel(remote_port=remote_port, remote_host=remote_host, local_port=local_port)
         self.clients = 0
 
